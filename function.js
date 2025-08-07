a9fd2b867c9e25ede8378d60e880c4b2ed6a2afb
maruf-hussain <engmarufhussain@gmail.com>
2025-08-07 15:25:51 +0600

    function my school

--- function.js
+++ function.js
@@ -11,10 +11,21 @@
 // };
 // add(10,20); // Output: 30
 
-function studentSallary(eat,noteat){
-    const eatTotal = 2500 * eat;
-    const noteatTotal = 1200 * noteat;
-    const total = eatTotal + noteatTotal;
-    console.log(total)
+// function studentSallary(eat,noteat){
+//     const eatTotal = 2500 * eat;
+//     const noteatTotal = 1200 * noteat;
+//     const total = eatTotal + noteatTotal;
+//     console.log(total)
+// };
+// studentSallary(3,25);
+
+
+function myschollBeton(abasik,onabasik){
+    const betonAbasik = 2500 * abasik;
+    const betonOnabasik = 1200 * onabasik;
+    const totalBeton = betonAbasik + betonOnabasik;
+    return totalBeton;
+
 };
-studentSallary(3,25);
+const totalTaka = myschollBeton(3,25);
+console.log(totalTaka); // Output: 33000
