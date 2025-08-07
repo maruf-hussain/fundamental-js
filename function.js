71027e0ae828fb5bced90057e4a9a2ea7740f790
maruf-hussain <engmarufhussain@gmail.com>
2025-08-07 15:17:22 +0600

    function my school

---
+++ function.js
@@ -0,0 +1,20 @@
+// function squre(number){
+//     return number * 10;
+// };
+
+// const total = squre(100);
+// console.log(total); // Output: 1000
+
+// function add(num1,num2){
+//     const total = num1 + num2;
+//     console.log(total);
+// };
+// add(10,20); // Output: 30
+
+function studentSallary(eat,noteat){
+    const eatTotal = 2500 * eat;
+    const noteatTotal = 1200 * noteat;
+    const total = eatTotal + noteatTotal;
+    console.log(total)
+};
+studentSallary(3,25);
