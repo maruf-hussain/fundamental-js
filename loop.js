7b5e8d1b323a7921b9d6758cde08aa4fa163eb86
maruf-hussain <engmarufhussain@gmail.com>
2025-08-08 22:09:16 +0600

    shahanara height

---
+++ loop.js
@@ -0,0 +1,4 @@
+for( let i = 0; i < 10; i++){
+    console.log(i);
+} // Output: 150    
+// Output: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
---
+++ problem-solving.js
@@ -0,0 +1,6 @@
+function shahanaraHeight(ince){
+    const feet = ince / 12;
+    return feet;
+};
+const total = shahanaraHeight(65);
+console.log(total); // Output: 5    
