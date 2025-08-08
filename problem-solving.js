8353c3c1d98687ba9c2a319b334fc3e871d96928
maruf-hussain <engmarufhussain@gmail.com>
2025-08-08 22:16:23 +0600

    shahanara height

--- problem-solving.js
+++ problem-solving.js
@@ -1,6 +1,14 @@
+// function shahanaraHeight(ince){
+//     const feet = ince / 12;
+//     return feet;
+// };
+// const total = shahanaraHeight(65);
+// console.log(total); // Output: 5    
+
 function shahanaraHeight(ince){
-    const feet = ince / 12;
-    return feet;
+    const feet = parseInt(ince / 12);
+    const remaining = ince % 12;
+    return feet + ' feet ' + remaining + ' inches';
 };
-const total = shahanaraHeight(65);
+const total = shahanaraHeight(75);
 console.log(total); // Output: 5    
