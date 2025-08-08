056af08a2a1dbe068c13b1c201cff237f9221a9e
maruf-hussain <engmarufhussain@gmail.com>
2025-08-08 16:29:54 +0600

    arrayfunction

--- function.js
+++ function.js
@@ -20,12 +20,37 @@
 // studentSallary(3,25);
 
 
-function myschollBeton(abasik,onabasik){
-    const betonAbasik = 2500 * abasik;
-    const betonOnabasik = 1200 * onabasik;
-    const totalBeton = betonAbasik + betonOnabasik;
-    return totalBeton;
+// function myschollBeton(abasik,onabasik){
+//     const betonAbasik = 2500 * abasik;
+//     const betonOnabasik = 1200 * onabasik;
+//     const totalBeton = betonAbasik + betonOnabasik;
+//     return totalBeton;
 
+// };
+// const totalTaka = myschollBeton(3,25);
+// console.log(totalTaka); // Output: 33000
+
+
+// function studentBeton(khawa,nakhawa){
+//     const khawaBeton = 2500 * khawa;
+//     const nakhawaBeton = 1200 * nakhawa;
+//     const totalBeton = khawaBeton + nakhawaBeton;
+//     return totalBeton;
+// };
+// const total = studentBeton(3,25);
+// console.log(total); // Output: 33000
+
+
+function arraySum(numbers){
+    let sum = 0;
+    for(const number of numbers){
+        sum = sum + number;
+    }
+    return sum;
 };
-const totalTaka = myschollBeton(3,25);
-console.log(totalTaka); // Output: 33000
+
+// const num = [10, 20, 30, 40, 50];
+const total = arraySum([10, 20, 30, 40, 50]);
+console.log(total); // Output: 150
+
+
